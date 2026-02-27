adf70a097f25ecbb99d0c2c7073ab97f843cee75
Meri Adrião <gamadriao@gmail.com>
2026-02-27 11:14:48 -0300

    Add @vercel/analytics package and import in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef, useMemo } from "react";
 import "./index.css";
 
+import { Analytics } from "@vercel/analytics/next"
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
